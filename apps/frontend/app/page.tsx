ebb76fb34c34c298fd8ceac7bec50a5536ced278
Shashwat-916 <shashwatdev4009@gmail.com>
2026-07-21 21:11:59 +0530

    fix: track apps/frontend as regular files instead of submodule pointer

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className="text-4xl font-bold text-center mt-20"
+    > Scribble frontend</div>
+  )
+}
